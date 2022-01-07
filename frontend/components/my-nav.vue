22f0d778eb7ea368c2752936274be5fddaf50b80
ngzm <nk.ngzm@gmail.com>
2022-01-07 17:26:45 +0900

    フロントエンド環境初期構築

---
+++ frontend/components/my-nav.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <header>
+      <div>
+        <div>
+          <h1>
+            <nuxt-link to="/">Nuxt Cafe</nuxt-link>
+          </h1>
+          <div>
+            <!-- TODO: ハンバーガーメニュー実装 -->
+          </div>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <nuxt-link to="/shop">店舗情報</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/menu">メニュー</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/information">お知らせ</nuxt-link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'MyNav',
+  setup() {
+    return {}
+  },
+})
+</script>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <my-nav />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import MyNav from '@/components/my-nav.vue'
+
+export default defineComponent({
+  name: 'MyIndex',
+  components: { MyNav },
+})
+</script>
