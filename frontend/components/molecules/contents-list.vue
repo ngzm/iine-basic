18aa996b5bcc976bafa27b037ba768a3da0d9380
ngzm <nk.ngzm@gmail.com>
2022-05-15 18:28:42 +0900

    news list コンポーネントを list コンポーネントに汎用化

---
+++ frontend/components/molecules/contents-list.vue
@@ -0,0 +1,41 @@
+<template>
+  <ul class="content-list">
+    <li v-for="content in contents" :key="content.id">
+      <div class="edit-activator">
+        <slot name="editActivator" :content="content" />
+      </div>
+      <div>
+        <slot :content="content" />
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from '@nuxtjs/composition-api'
+import { ContentType } from '@/types/content-types'
+
+export default defineComponent({
+  name: 'NewsList',
+  props: {
+    contents: {
+      type: Array as PropType<ContentType[]>,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+ul.content-list {
+  li {
+    margin-top: 0.5rem;
+    position: relative;
+    .edit-activator {
+      position: absolute;
+      top: 0;
+      left: -2rem;
+    }
+  }
+}
+</style>
