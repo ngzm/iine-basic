a64a505ab9263626e6fd55b7d9d21b25bd56cf20
ngzm <nk.ngzm@gmail.com>
2022-05-19 23:05:41 +0900

    デプロイ環境整備

--- create-iine-user.sh
+++ create-iine-user.sh
@@ -3,7 +3,7 @@
 # 開発環境 mongodb iine ユーザ作成処理
 #   cf.mome/mongodb_環境構築メモ.txt
 #
-docker-compose exec mongo sh -c '
+docker compose exec mongo sh -c '
   mongosh -u ${MONGO_INITDB_ROOT_USERNAME}      \
           -p ${MONGO_INITDB_ROOT_PASSWORD}      \
           --authenticationDatabase admin        \
